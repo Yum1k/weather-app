959929ea5b87779e8ec55e7468e95072d3ac25b7
Dmitry <yumik00@yandex.ru>
2025-05-27 15:16:36 +0400

    Commit №3

--- Weather App/obj/Debug/net8.0/Weather App.AssemblyInfo.cs
+++ Weather App/obj/Debug/net8.0/Weather App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Weather App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2912e58196863b2efbb50d729f9507124baf1253")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d94ef6d316c73b4fb7471a20a6e716a1fc85070")]
 [assembly: System.Reflection.AssemblyProductAttribute("Weather App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Weather App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
